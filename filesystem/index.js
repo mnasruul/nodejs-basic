4802e6645f933f4e553a20d1471dc959171d88b4
mnasruul <mnasruul@gmail.com>
2022-08-15 23:17:24 +0700

    filesystem event

---
+++ filesystem/index.js
@@ -0,0 +1,6 @@
+// TODO: tampilkan teks pada notes.txt pada console.
+const fs = require('fs');
+const path = require('path');
+const filePath = path.resolve(__dirname, 'notes.txt'); 
+const data = fs.readFileSync(filePath, 'UTF-8');
+console.log(data);
